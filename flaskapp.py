9746c4e4d58f0374bf620c32e787e41d969318d8
lrouker <rouker.1@outlook.com>
2022-06-19 13:56:29 -0400

    Working redis

--- flaskapp.py
+++ flaskapp.py
@@ -1,9 +1,16 @@
 from flask import Flask, request
+import redis
 app = Flask(__name__)
+r = redis.Redis(host='redis', port=6379, decode_responses=True)
 
-@app.route('/', methods=['GET'])
+@app.route('/api/reminders', methods=['GET'])
 def handle_get():
-    return "Hello world"
+    return r.get("reminder2")
+
+@app.route('/api/reminders', methods=['POST'])
+def handle_post():
+    r.mset({"reminder1": "Hello", "reminder2": "World"})
+    return "Hello world POST"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
