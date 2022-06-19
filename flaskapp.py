26a6b1efeafe140e9d83c1f65746e99d85c6e914
lrouker <rouker.1@outlook.com>
2022-06-19 14:22:48 -0400

    2:22 - this meets the minimum bar, now to refactor

--- flaskapp.py
+++ flaskapp.py
@@ -1,16 +1,21 @@
-from flask import Flask, request
-import redis
+from flask import Flask, request, jsonify
+import redis, json, uuid
 app = Flask(__name__)
 r = redis.Redis(host='redis', port=6379, decode_responses=True)
 
 @app.route('/api/reminders', methods=['GET'])
 def handle_get():
-    return r.get("reminder2")
+    retList = []
+    for key in r.scan_iter():
+      retList.append(json.loads(r.get(key)))
+    return jsonify(retList)
 
 @app.route('/api/reminders', methods=['POST'])
 def handle_post():
-    r.mset({"reminder1": "Hello", "reminder2": "World"})
-    return "Hello world POST"
+    data = request.data
+    id = str(uuid.uuid1())
+    r.mset({id: data})
+    return "Accepted"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
